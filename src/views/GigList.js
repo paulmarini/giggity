4de3c4342c4adb170944f6483e8c45f7fb1816b7
Greg <greg>
2019-04-27 20:04:12 -0700

    refactor sidebar

--- src/views/GigList.js
+++ src/views/GigList.js
@@ -2,23 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { actions } from '../store';
 import { Link } from 'react-router-dom'
-import { Drawer, List, ListItem, ListItemText, ListItemIcon, Link as MUILink } from '@material-ui/core';
-import UserAvailability from './UserAvailability';
+import { ListItem, ListItemText, ListItemIcon, Link as MUILink } from '@material-ui/core';
+import UserAvailability from '../components/UserAvailability';
 import { gigService, userService, emit } from '../socket'
 import { withStyles } from '@material-ui/core/styles';
 import moment from 'moment';
 
 const styles = theme => ({
-  toolbar: theme.mixins.toolbar,
-  modal: {
-    zIndex: theme.zIndex.modal - 2
-  },
   title: {
     overflow: 'hidden',
     textOverflow: 'ellipsis'
-  },
-  selected: {
-    backgroundColor: 'pink'
   }
 });
 
@@ -50,10 +43,46 @@ class GigList extends Component {
       .then(res => this.props.loadUserAvailability(res.data));
   }
 
-  renderContents() {
-    const { gigsList, currentGig, userAvailability, currentUser, classes } = this.props;
+  renderGigItem = (gig) => {
+    const { currentGig, userAvailability, currentUser, classes } = this.props;
+    const date = moment(gig.start).format('MM/DD');
     return (
-      <List style={{ width: this.props.width }}>
+      <ListItem
+        button
+        divider
+        key={gig._id}
+        component={Link}
+        selected={currentGig._id === gig._id}
+        to={`/gigs/${gig._id}`}
+        alignItems="flex-start"
+      >
+        <ListItemIcon>
+          <b>{date}</b>
+        </ListItemIcon>
+        <ListItemText
+          primaryTypographyProps={{ className: classes.title }}
+          secondaryTypographyProps={{ component: 'div' }}
+          primary={
+            <MUILink
+              component='span'
+            >
+              {gig.name}
+            </MUILink>
+          }
+          secondary={
+            <UserAvailability
+              userId={currentUser.userId}
+              gigId={gig._id} availability={userAvailability[gig._id]}
+            />
+          } />
+      </ListItem>
+    );
+  }
+
+  render() {
+    const { gigsList, currentGig } = this.props;
+    return (
+      <>
         <ListItem
           button
           component={Link}
@@ -63,72 +92,14 @@ class GigList extends Component {
           <ListItemText primary="New Gig!" />
         </ListItem>
         {
-          gigsList.map(gig => {
-            return (
-              <ListItem
-                button
-                divider
-                key={gig._id}
-                component={Link}
-                selected={currentGig._id === gig._id}
-                to={`/gigs/${gig._id}`}
-                alignItems="flex-start"
-                classes={{
-                  selected: classes.selected
-                }}
-              >
-                <ListItemIcon>
-                  <b>
-                    {moment(gig.start).format('MM/DD')}
-                  </b>
-                </ListItemIcon>
-                <ListItemText
-                  primaryTypographyProps={{ className: classes.title }}
-                  secondaryTypographyProps={{ component: 'div' }}
-                  primary={
-                    <MUILink
-                      component='span'
-                    >
-                      {gig.name}
-                    </MUILink>
-                  }
-                  secondary={
-                    <UserAvailability
-                      userId={currentUser.userId}
-                      gigId={gig._id} availability={userAvailability[gig._id]}
-                    />
-                  } />
-              </ListItem>
-            );
-          })
+          gigsList.map(this.renderGigItem)
         }
-      </List>
+      </>
     );
-  }
-
-  render() {
-    const { width, classes, drawerOpen, drawerWidth } = this.props;
-    return (
-      <Drawer
-        anchor="left"
-        className='gig-list'
-        variant={width === 'xs' ? 'temporary' : 'permanent'}
-        open={width !== 'xs' || drawerOpen}
-        classes={{ modal: classes.modal }}
-        PaperProps={{
-          style: {
-            width: drawerWidth
-          }
-        }}
-      >
-        <div className={classes.toolbar} />
-        {this.renderContents()}
-      </Drawer>
-    )
   }
 }
 
 export default connect(
-  state => ((({ gigsList, currentGig, currentUser, userAvailability, drawerOpen }) => ({ gigsList, currentGig, currentUser, userAvailability, drawerOpen }))(state)),
+  state => ((({ gigsList, currentGig, currentUser, userAvailability }) => ({ gigsList, currentGig, currentUser, userAvailability }))(state)),
   { loadGigs: actions.loadGigs, loadUserAvailability: actions.loadUserAvailability }
 )(withStyles(styles)(GigList));
