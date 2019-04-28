4998fc1069b6f97934214c2d58b8bb1ebd4ce097
Greg <greg>
2019-04-28 01:51:14 -0700

    adding build app

---
+++ build/precache-manifest.09bc1e954ef74932280dac5746df1647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "86c976dee5659df88784",
+    "url": "/static/js/main.84ddc86b.chunk.js"
+  },
+  {
+    "revision": "e112edfcee91276fee44",
+    "url": "/static/js/2.db86aa88.chunk.js"
+  },
+  {
+    "revision": "86c976dee5659df88784",
+    "url": "/static/css/main.ade66e96.chunk.css"
+  },
+  {
+    "revision": "fa6953d687d8060c1a6cd1b66a84c3ab",
+    "url": "/index.html"
+  }
+];
